62d0691c97ab6c5aeb7c5a42b1e01b4f1ba94586
Milind..Singh <86865158+YoutubeSmokey@users.noreply.github.com>
2022-03-07 14:05:51 +0530

    Add files via upload

---
+++ DiscordBot/main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+	guild_count = 0
+
+	for guild in bot.guilds:
+		print(f"- {guild.id} (name: {guild.name})")
+
+		guild_count = guild_count + 1
+
+	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+@bot.event
+async def on_message(message):
+	if message.content == "hello":
+		await message.channel.send("Hey My Firend")
+		
+
+bot.run(DISCORD_TOKEN)
